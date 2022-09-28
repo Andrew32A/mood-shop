ca02de372c53dfd76782b3937025338bd61ccaa9
Andrew Alsing <111094607+Andrew32A@users.noreply.github.com>
2022-09-27 21:14:30 -0700

    displayed cart items on console

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ import data from './data.js'
 const itemsContainer = document.querySelector('#items')
 const cart = [] // array to hold cart items
 
+
 // length of data determines how many times loop is ran
 for (let i = 0; i < data.length; i += 1) {
 	// create new div element and give it class name
@@ -44,16 +45,65 @@ for (let i = 0; i < data.length; i += 1) {
 
 
 
-// add items function
+// adds items to cart
 function addItem(name, price) {
+    for (let i = 0; 1 < cart.length; i += 1) {
+        if (cart[i].name === name) {
+            cart[i].qty += 1
+            return
+        }
+    }
+
     const item = {name: name, price: price, qty: 1}
-    cart.push(name)
+    cart.push(item)
 }
 
 
-// show items function
+// shows items in cart
 function showItems() {
-    console.log(cart[0])
+    console.log(`You have ${getQty()} items in your cart`) // could also make a 'const qty = getQty()' line and put qty in the string
+
+    for (let i = 0; i < cart.length; i += 1) {
+        console.log(`${cart[i].name} $${cart[i].price} x ${cart[i].qty}`)
+    }
+
+    console.log(`Your total in cart is: $${getTotal()}`)
 }
+
+// gets quantity of total cart items
+function getQty () {
+    let qty = 0
+    for (let i = 0; i < cart.length; i += 1) {
+        qty += cart[i].qty
+    }
+    return qty
+}
+
+// gets total price of cart items
+function getTotal () {
+    let total = 0
+    for (let i = 0; i < cart.length; i += 1) {
+        total += cart[i].price * cart[i].qty
+    }
+    return total.toFixed(2) // the '2' rounds to 2 decimals, without it the float would be extremely long
+}
+
+addItem("apple", 0.99) 
+addItem("banana", 2.99)
+addItem("banana", 2.99)
+addItem("banana", 2.99)
+addItem("banana", 2.99)
+addItem("apple", 0.99) 
+
 
 showItems()
+
+
+
+// extra notes
+
+// keys and objects
+// const a = 100
+// const b = 200
+// const obj = {a, b}
+// console.log(obj)
