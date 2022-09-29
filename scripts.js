b2639b7a4dc91d0309d539de97ad464b6e04527a
Andrew Alsing <111094607+Andrew32A@users.noreply.github.com>
2022-09-28 23:53:50 -0700

    cart fully functional

--- scripts.js
+++ scripts.js
@@ -19,7 +19,7 @@ for (let i = 0; i < data.length; i += 1) {
 	img.src = data[i].image
 	img.width = 300
 	img.height = 300
-	// Add the image to the div
+	// add the image to the div
 	newDiv.appendChild(img)
 
 	// put new div inside items container
@@ -42,13 +42,13 @@ for (let i = 0; i < data.length; i += 1) {
 	button.id = data[i].name
 	// creates a custom attribute called data-price. that will hold price for each element in the button
 	button.dataset.price = data[i].price
-	button.innerHTML = "Add to Cart"
+	button.innerHTML = 'Add to Cart'
 	newDiv.appendChild(button)
 }
 
 // ---------------------------------------------------
 // selects all buttons
-const all_items_button = Array.from(document.querySelectorAll("button"))
+const all_items_button = Array.from(document.querySelectorAll('button'))
 // console.log(all_items_button)
 
 all_items_button.forEach(elt => elt.addEventListener('click', () => {
@@ -57,15 +57,44 @@ all_items_button.forEach(elt => elt.addEventListener('click', () => {
     }))
 
 // ---------------------------------------------------
+// handle change events on update input
+itemList.onchange = function(e) {
+    if (e.target && e.target.classList.contains('update')) {
+        // console.log(e)
+        const name = e.target.dataset.name
+        const qty = parseInt(e.target.value) // parseInt takes in a string and returns a number (interger)
+        updateCart(name, qty)
+    }
+}
+
+// ---------------------------------------------------
+// handle clicks on list
+itemList.onclick = function(e) {
+    // console.log("clicked list!!")
+    // console.log(e.target)
+
+    if (e.target && e.target.classList.contains('remove')) {
+        const name = e.target.dataset.name // data-name="????"
+        removeItem(name)
+    } else if (e.target && e.target.classList.contains('add-one')) {
+        const name = e.target.dataset.name
+        addItem(name)
+    } else if (e.target && e.target.classList.contains('remove-one')) {
+        const name = e.target.dataset.name
+        removeItem(name, 1)
+    }
+}
+
+// ---------------------------------------------------
 // adds items to cart
 function addItem(name, price) {
     for (let i = 0; i < cart.length; i += 1) {
         if (cart[i].name === name) {
             cart[i].qty += 1
+            showItems()
             return
         }
     }
-
     const item = {name, price, qty: 1}
     cart.push(item)
 }
@@ -86,7 +115,13 @@ function showItems() {
         // {name:'Apple', price: 0.99, qty: 3}
         const {name, price, qty} = cart[i]
 
-        itemStr += `<li> ${name} $${price} x ${qty} = $${qty * price} </li>`
+        itemStr += `<li> 
+        ${name} $${price} x ${qty} = $${qty * price} 
+        <button class="remove" data-name="${name}">Remove</button> 
+        <button class="add-one" data-name="${name}"> + </button> 
+        <button class="remove-one" data-name="${name}"> - </button> 
+        <input class="update" type="number" min="0" data-name="${name}" placeholder="Quantity">
+        </li>`
     }
     itemList.innerHTML = itemStr
 
@@ -125,39 +160,52 @@ function removeItem(name, qty = 0) {
             if (cart[i].qty < 1 || qty === 0) {
                 cart.splice(i, 1)
             }
+            showItems()
             return
         }
     }
 }
 
-
-addItem('Apple', 0.99)
-addItem('Orange', 1.29)
-addItem('Orange', 1.29)
-addItem('test', 0.69)
-
-addItem('Apple', 0.99)
-addItem('Orange', 1.29)
-addItem('Orange', 1.29)
-addItem('test', 0.69)
-
-addItem('Apple', 0.99)
-addItem('Orange', 1.29)
-addItem('Orange', 1.29)
-addItem('test', 0.69)
-
-
-removeItem('Orange', 1)
-
-
-
-
-
+// ---------------------------------------------------
+// updates cart
+function updateCart(name, qty) {
+    for (let i = 0; i < cart.length; i += 1) {
+        if (cart[i].name === name) {
+            if (qty < 1) {
+                removeItem(name)
+                return
+            }
+            cart[i].qty = qty
+            showItems()
+            return
+        }
+    }
+}
 
 showItems()
 
+// ---------------------------------------------------
+// adds items to website for testing purposes
+
+// addItem('Apple', 0.99)
+// addItem('Orange', 1.29)
+// addItem('Orange', 1.29)
+// addItem('test', 0.69)
+
+// addItem('Apple', 0.99)
+// addItem('Orange', 1.29)
+// addItem('Orange', 1.29)
+// addItem('test', 0.69)
+
+// addItem('Apple', 0.99)
+// addItem('Orange', 1.29)
+// addItem('Orange', 1.29)
+// addItem('test', 0.69)
 
 
+// removeItem('Orange', 1)
+
+// ---------------------------------------------------
 // extra notes
 
 // keys and objects
@@ -165,3 +213,6 @@ showItems()
 // const b = 200
 // const obj = {a, b}
 // console.log(obj)
+
+
+// data-"id" is a developer attribute, kinda like a custom class
